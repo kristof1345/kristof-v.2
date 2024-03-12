bf35411e6a982ebe0ae0b39af226c2c12ee3881d
kristof1345 <kristofnemeth1345@gmail.com>
2024-03-12 09:31:58 +0100

    g

--- src/pages/components/Post.js
+++ src/pages/components/Post.js
@@ -2,6 +2,10 @@ import React from "react";
 import parse from "html-react-parser";
 
 export default function Post(post) {
+  if (post.post.title === undefined) {
+    post.post.title = "";
+  }
+
   return (
     <article>
       <h2>{post.post.title === undefined ? null : post.post.title}</h2>
