6bf59f1e2c535aebad150105ce24ac00cdc9f978
kristof1345 <kristofnemeth1345@gmail.com>
2024-03-12 09:34:06 +0100

    d

--- src/pages/components/Post.js
+++ src/pages/components/Post.js
@@ -8,7 +8,7 @@ export default function Post(post) {
 
   return (
     <article>
-      <h2>{post.post.title === undefined ? null : post.post.title}</h2>
+      {/* <h2>{post.post.title === undefined ? null : post.post.title}</h2> */}
       <div className="post-content">
         {post.post.content === undefined ? null : parse(post.post.content)}
       </div>
