38d5fa8437daf1cef84b8a0a2051ca352e7b8339
kristof1345 <kristofnemeth1345@gmail.com>
2024-03-15 22:35:28 +0100

    ok

--- src/pages/components/OutOfPages.js
+++ src/pages/components/OutOfPages.js
@@ -5,7 +5,7 @@ const OutOfPages = () => {
     <div>
       <h1>Slow dooooown. You have gone way too far.</h1>
       <p>There are no more pages here.</p>
-      <p>Please click the "Previous Posts" button</p>
+      <p>Please click the &apos;Previous Posts&apos; button</p>
     </div>
   );
 };
